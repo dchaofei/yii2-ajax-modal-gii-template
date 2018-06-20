1a414cad20336e99d0b7e573a58c950bb7fdba2a
caohui123 <firstcaohui@qq.com>
2018-06-20 17:43:28 +0800

    Update ajax-modal.php

--- view/ajax-modal.php
+++ view/ajax-modal.php
@@ -12,13 +12,15 @@ yii\bootstrap\Modal::end();
 ?>
 
 <script type="text/javascript">
-    $(function(){
+    <?php $this->beginBlock('js_end') ?>
+
+    $(function () {
         //get the click of modal button to create / update item
         //we get the button by class not by ID because you can only have one id on a page and you can
         //have multiple classes therefore you can have multiple open modal buttons on a page all with or without
         //the same link.
 //we use on so the dom element can be called again if they are nested, otherwise when we load the content once it kills the dom element and wont let you load anther modal on click without a page refresh
-        $(document).on('click', '.showModalButton', function(){
+        $(document).on('click', '.showModalButton', function () {
             //check if the modal is open. if it's open just reload content not whole modal
             //also this allows you to nest buttons inside of modals to reload the content it is in
             //the if else are intentionally separated instead of put into a function to get the
@@ -28,15 +30,18 @@ yii\bootstrap\Modal::end();
                 $('#modal').find('#modalContent')
                     .load($(this).attr('value'));
                 //dynamiclly set the header for the modal
-                document.getElementById('modalHeader').innerHTML = '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>' +  '<h4>' + $(this).attr('title') + '</h4>';
+                document.getElementById('modalHeader').innerHTML = '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>' + '<h4>' + $(this).attr('title') + '</h4>';
             } else {
                 //if modal isn't open; open it and load content
                 $('#modal').modal('show')
                     .find('#modalContent')
                     .load($(this).attr('value'));
                 //dynamiclly set the header for the modal
-                document.getElementById('modalHeader').innerHTML = '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>' +  '<h4>' + $(this).attr('title') + '</h4>';
+                document.getElementById('modalHeader').innerHTML = '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>' + '<h4>' + $(this).attr('title') + '</h4>';
             }
         });
     });
+    <?php $this->endBlock() ?>
+
 </script>
+<?php $this->registerJs($this->blocks['js_end'], \yii\web\View::POS_END); ?>
